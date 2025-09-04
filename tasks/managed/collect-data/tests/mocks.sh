7631b2a2b197ca26825280ccc3c2dfd01f214894
Jing Qi <jinqi@redhat.com>
2025-09-04 14:31:48 +0800

    feat(RELEASE-1442): allow merging tags array
    Signed-off-by: Jing Qi <jinqi@redhat.com>
    
    This PR is to allow merging default tags array from Release, ReleasePlan
    and ReleasePlanAdmission.

--- tasks/managed/collect-data/tests/mocks.sh
+++ tasks/managed/collect-data/tests/mocks.sh
@@ -8,3 +8,42 @@ curl() {
   echo "$@" >> "$(params.dataDir)/mock_curl.txt"
   echo '{ "sha": "12345"}'
 }
+
+merge-json1() {
+  # Store the JSON strings in variables
+  json1="$1"
+  json2="$2"
+
+  # Validate JSON input
+  if ! echo "$json1" | jq -e . >/dev/null 2>&1; then
+    echo "Error: Invalid JSON in parameter 1"
+    exit 1
+  fi
+
+  if ! echo "$json2" | jq -e . >/dev/null 2>&1; then
+    echo "Error: Invalid JSON in parameter 2"
+    exit 1
+  fi
+
+  # Merge the JSON objects recursively, combining arrays and replacing other values
+  merged_json=$(printf '%s\n%s' "$json1" "$json2" | jq -cs '
+  def merge_objects(a; b):
+    a as $a | b as $b |
+    ($a | keys) + ($b | keys) | unique | map({
+      key: .,
+      value: (
+        if ($a[.] | type) == "object" and ($b[.] | type) == "object" then
+          merge_objects($a[.]; $b[.])
+        elif ($a[.] | type) == "array" and ($b[.] | type) == "array" then
+	  ($a[.] + $b[.]) | unique
+        else
+          $b[.] // $a[.]
+        end
+      )
+    }) | from_entries;
+
+  .[0] as $first | .[1] as $second | merge_objects($first; $second)
+  ')
+  # Print the merged JSON
+  echo "$merged_json"
+}
